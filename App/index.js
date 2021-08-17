4a35bb0a236210153b6ecd2f22611551089a1b4f
tarun-soni <soni.tarun26@gmail.com>
2021-08-17 15:28:07 +0530

    rendered  basic list of todos

--- App/index.js
+++ App/index.js
@@ -1,21 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react'
+import Options from './screens/Options'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello There Yo</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default () => <Options />
